527e77ee9519556b7c393d44d29550171e2387f0
sahibzadafahad-whizpool <sahibzadafahad99@gmail.com>
2022-04-18 11:24:57 +0500

    db

--- API/index.js
+++ API/index.js
@@ -1,5 +1,14 @@
 var express = require('express')
+const mysql = require("mysql")
 var app = express()
+
+const connection = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "pass",
+  database: process.env.MYSQL_DATABASE || "mydb",
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World! A new change is made. Change added')
