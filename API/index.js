e3fff8c57a0faa514412f87eb72067d6624b5f42
Hamza_master <Hamzakhalid0@github.com>
2022-04-19 03:12:23 -0700

    ss

--- API/index.js
+++ API/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2')
 var app = express()
 
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: '192.168.0.117',
   user: 'user',
   password: 'pass',
   database: 'reactdb'
@@ -12,6 +12,20 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err
   console.log('You are now connected...')
+
+  connection.query('CREATE TABLE people(id int primary key, name varchar(255), age int, address text)', function(err, result) {
+    if (err) throw err
+    connection.query('INSERT INTO people (id, name, age, address) VALUES (?, ?, ?, ?)', ['01', 'Larry', '41', 'California, USA'], function(err, result) {
+      if (err) throw err
+      connection.query('SELECT * FROM people', function(err, results) {
+        if (err) throw err
+        console.log(results[0].id)
+        console.log(results[0].name)
+        console.log(results[0].age)
+        console.log(results[0].address)
+      })
+    })
+  })
 })
 
 app.get('/', function (req, res) {
